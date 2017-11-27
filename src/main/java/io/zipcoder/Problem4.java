67518926755b669ca587af85a958688603c29c5e
lina <you@example.com>
2017-11-27 16:13:41 -0500

    wasn't me

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
 public class Problem4 {
+    static final int NO_OF_CHARS = 256;
+    static boolean canFormPalindrome(String str) {
+        // Create a count array and initialize
+        // all values as 0
+        int[] count = new int[NO_OF_CHARS];
+
+        // For each character in input strings,
+        // increment count in the corresponding count array
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i)]++;
+        // Count odd occurring characters
+        int odd = 0;
+        for (int i = 0; i < NO_OF_CHARS; i++) {
+            if ((count[i] & 1) != 0)
+                odd++;
+
+            if (odd > 1)
+                return false;
+        }
+
+        // Return true if odd count is 0 or 1,
+        return true;
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,25 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+    @Test
+    public void testCanFormPalindrome(){
+        boolean expected = true;
+        boolean actual = Problem4.canFormPalindrome("aaabbbb");
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void testCanFormPalindrome2(){
+        boolean expected = false;
+        boolean actual= Problem4.canFormPalindrome("cdefghmnopqrstuvw");
+        Assert.assertEquals(expected,actual);
+    }
+    @Test
+    public void testCanFormPalindrome3(){
+        boolean expected = true;
+        boolean actual = Problem4.canFormPalindrome("cdcdcdcdeeeef");
+        Assert.assertEquals(expected,actual);
+    }
 }
